86d38758370a8b88ef1e685ed0f103aafe415510
Luis Pedro Morales <luismorarod98@gmail.com>
2022-09-16 20:59:39 -0600

    feat(mongoDB): add connection to DB hosted in Mongo Cloud

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.MONGODB_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
 
 const app = express();
-
-
 app.use(express.json());
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log(`Server Started at ${3000}`)
 })
